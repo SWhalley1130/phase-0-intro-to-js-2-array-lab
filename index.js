189afddccd64e8fa00f0eaef7dfc1ef9db9bb6d5
Sarah Whalley <s.whalley1130@gmail.com>
2023-01-11 14:32:44 -0700

    Adds push and unshift functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)
+{
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name);
+}
+
